4794a92578933cf3aba66f28ffc2dc122205b9ac
Kelly Liu <kellyliu@usc.edu>
2018-01-28 00:35:39 -0800

    Moved JS to external file

---
+++ mail_signup_sync.js
@@ -0,0 +1,17 @@
+// This JavaScript code comes from MailChimp's original code
+(function($) {
+	window.fnames = new Array();
+	window.ftypes = new Array();
+	fnames[0]='EMAIL';
+	ftypes[0]='email';
+	fnames[1]='FNAME';
+	ftypes[1]='text';
+	fnames[2]='LNAME';
+	ftypes[2]='text';
+	fnames[6]='MMERGE6';
+	ftypes[6]='radio';
+	fnames[3]='MMERGE3';
+	ftypes[3]='radio';
+}
+(jQuery));
+var $mcj = jQuery.noConflict(true);
