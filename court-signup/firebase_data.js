00f36ae84db201bd31aafc8b7c39b1e6b833d8a7
Kelly Liu <kellyliu@usc.edu>
2018-03-03 18:08:46 -0800

    Moved JavaScript to separate file

---
+++ court-signup/firebase_data.js
@@ -0,0 +1,10 @@
+//Firebase initialization code snippet
+var config = {
+  apiKey: "AIzaSyBNjJlXJY7VibkQJDh-ltuUOPPY_tCXWVU",
+  authDomain: "sc-badminton-court-signup.firebaseapp.com",
+  databaseURL: "https://sc-badminton-court-signup.firebaseio.com",
+  projectId: "sc-badminton-court-signup",
+  storageBucket: "sc-badminton-court-signup.appspot.com",
+  messagingSenderId: "685908240401"
+};
+firebase.initializeApp(config);
